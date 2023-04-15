db10514deeb8e8bff32ef941d6fbe429521f72c8
Victor Silva <victor.silvaPC@hotmail.com>
2023-04-15 08:09:34 -0300

    feat: :zap: solved the salary_bonus challenge

---
+++ dart/beginner/salary_with_bonus.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  String salesmanName = stdin.readLineSync().toString().trim();
+  double fixedSalary = double.parse(stdin.readLineSync().toString().trim());
+  double salesAmount = double.parse(stdin.readLineSync().toString().trim());
+
+  String totalSalary = ((0.15 * salesAmount) + fixedSalary).toStringAsFixed(2);
+
+  print("TOTAL = $totalSalary");
+}
