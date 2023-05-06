bf009df414302eff4ea25b43f1d6b794637b536f
Victor Silva <victor.silvaPC@hotmail.com>
2023-05-06 13:05:18 -0300

    fix: :zap: fixed the 1038 challenge

--- dart/beginner/1038.dart
+++ dart/beginner/1038.dart
@@ -1,1 +1,38 @@
-void main(List<String> args) {}
+import 'dart:io';
+
+void main(List<String> args) {
+  final List<int> inputs = stdin
+      .readLineSync()
+      .toString()
+      .trim()
+      .split(" ")
+      .map((e) => int.parse(e))
+      .toList();
+
+  int code = inputs[0];
+  int amount = inputs[1];
+
+  double productPrice = 0.00;
+
+  switch (code) {
+    case 1:
+      productPrice = 4.00;
+      break;
+    case 2:
+      productPrice = 4.50;
+      break;
+    case 3:
+      productPrice = 5.00;
+      break;
+    case 4:
+      productPrice = 2.00;
+      break;
+    case 5:
+      productPrice = 1.50;
+      break;
+  }
+
+  double totalPrice = productPrice * amount;
+
+  print("Total: R\$ ${totalPrice.toStringAsFixed(2)}");
+}
